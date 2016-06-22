678a804dc57cebf52a4d4a1e186dbe714593f2e5
l3aichaDev <mr.izedtea@gmail.com>
2016-06-22 13:44:21 +0700

    try catch

--- raw.php
+++ raw.php
@@ -4,34 +4,40 @@ include 'lib/PHPExcel/IOFactory.php';
 require_once 'config.php';
 require_once 'query.php';
 
-$h = $_GET['h'];
-$h2 = $h == 2 ? '1H2016' : '2H2016 (FC)';
-$fileName = ROOT_DIR.'/templates/raw.xlsx'; 
-$reader = PHPExcel_IOFactory::createReader('Excel2007');
-$excel = $reader->load($fileName); 
-$sheet = $excel->setActiveSheetIndex(0);
+try  {
 
-foreach ($db->query(Query::agency()) as $r) {
-    $sheet->setCellValueByColumnAndRow($r['seq'], 1, $r['id']);
-    $sheet->setCellValueByColumnAndRow($r['seq'], 2, $r['name']);
-}
+    $h = $_GET['h'];
+    $h2 = $h == 2 ? '1H2016' : '2H2016 (FC)';
+    $fileName = ROOT_DIR.'/templates/raw.xlsx'; 
+    $reader = PHPExcel_IOFactory::createReader('Excel2007');
+    $excel = $reader->load($fileName); 
+    $sheet = $excel->setActiveSheetIndex(0);
 
-foreach ($db->query(Query::disciplineConfig()) as $r) {
-    $sheet = $excel->setActiveSheetIndexByName($r['name']);
-
-    foreach ($db->query(Query::agency()) as $r1) {
-        $sheet->setCellValueByColumnAndRow($r1['seq'], 1, $r1['id']);
-        $sheet->setCellValueByColumnAndRow($r1['seq'], 2, $r1['name']);
+    foreach ($db->query(Query::agency()) as $r) {
+        $sheet->setCellValueByColumnAndRow($r['seq'], 1, $r['id']);
+        $sheet->setCellValueByColumnAndRow($r['seq'], 2, $r['name']);
     }
 
-    foreach ($db->query(Query::disciplineData($h, $r['name'])) as $r1) {
-        $sheet->setCellValueByColumnAndRow($r1['col'], $r1['row'], $r1['value']);
-    }
-}
+    foreach ($db->query(Query::disciplineConfig()) as $r) {
+        $sheet = $excel->setActiveSheetIndexByName($r['name']);
 
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'); 
-header('Content-Disposition: attachment;filename="RAW Disciplines Data by 56 Cat '.$h2.'.xlsx"'); 
-header('Cache-Control: max-age=0'); 
-$writer = PHPExcel_IOFactory::createWriter($excel, 'Excel2007'); 
-$writer->save('php://output');
+        foreach ($db->query(Query::agency()) as $r1) {
+            $sheet->setCellValueByColumnAndRow($r1['seq'], 1, $r1['id']);
+            $sheet->setCellValueByColumnAndRow($r1['seq'], 2, $r1['name']);
+        }
+
+        foreach ($db->query(Query::disciplineData($h, $r['name'])) as $r1) {
+            $sheet->setCellValueByColumnAndRow($r1['col'], $r1['row'], $r1['value']);
+        }
+    }
+
+    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'); 
+    header('Content-Disposition: attachment;filename="RAW Disciplines Data by 56 Cat '.$h2.'.xlsx"'); 
+    header('Cache-Control: max-age=0'); 
+    $writer = PHPExcel_IOFactory::createWriter($excel, 'Excel2007'); 
+    $writer->save('php://output');
+
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
 ?>
--- sub.php
+++ sub.php
@@ -4,43 +4,49 @@ include 'lib/PHPExcel/IOFactory.php';
 require_once 'config.php';
 require_once 'query.php';
 
-$h = $_GET['h'];
-$h2 = $h == 2 ? '1H2016' : '2H2016 (FC)';
-$disciplineName = $_GET['d']; //'Creative';
-$fileName = ROOT_DIR.'/templates/'.$disciplineName.'.xlsx'; 
-$reader = PHPExcel_IOFactory::createReader('Excel2007');
-$excel = $reader->load($fileName);
-$sheet = $excel->setActiveSheetIndexByName('Total');
+try  {
 
-foreach ($db->query(Query::agency()) as $r) {
-    $sheet->setCellValueByColumnAndRow($r['seq'], 1, $r['id']);
-    $sheet->setCellValueByColumnAndRow($r['seq'], 2, $r['name']);
+    $h = $_GET['h'];
+    $h2 = $h == 2 ? '1H2016' : '2H2016 (FC)';
+    $disciplineName = $_GET['d']; //'Creative';
+    $fileName = ROOT_DIR.'/templates/'.strtolower($disciplineName).'.xlsx'; 
+    $reader = PHPExcel_IOFactory::createReader('Excel2007');
+    $excel = $reader->load($fileName);
+    $sheet = $excel->setActiveSheetIndexByName('Total');
+
+    foreach ($db->query(Query::agency()) as $r) {
+        $sheet->setCellValueByColumnAndRow($r['seq'], 1, $r['id']);
+        $sheet->setCellValueByColumnAndRow($r['seq'], 2, $r['name']);
+    }
+
+    foreach ($db->query(Query::subDisciplineByDisciplineName(2, $disciplineName)) as $r) {
+        $sheet = $excel->setActiveSheetIndexByName($r['name']);
+
+        foreach ($db->query(Query::agency()) as $r1) {
+            $sheet->setCellValueByColumnAndRow($r1['seq'], 1, $r1['id']);
+            $sheet->setCellValueByColumnAndRow($r1['seq'], 2, $r1['name']);
+            $sheet->setCellValueByColumnAndRow($r1['seq'], 61, $r1['id']);
+            $sheet->setCellValueByColumnAndRow($r1['seq'], 62, $r1['name']);
+        }
+
+        foreach ($db->query(Query::subDisciplineData($h, $disciplineName, $r['name'])) as $r1) {
+            $sheet->setCellValueByColumnAndRow($r1['col'], $r1['row'], $r1['percent']);
+        }
+
+        foreach ($db->query(Query::disciplineData2($h, $disciplineName)) as $r1) {
+            $sheet->setCellValueByColumnAndRow($r1['col'], $r1['row'], $r1['value']);
+        }
+    }
+
+    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'); 
+    header('Content-Disposition: attachment;filename="RAW '.$disciplineName.' '.$h2.'.xlsx"'); 
+    header('Cache-Control: max-age=0'); 
+    $writer = PHPExcel_IOFactory::createWriter($excel, 'Excel2007'); 
+    $writer->save('php://output');
+
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
-
-foreach ($db->query(Query::subDisciplineByDisciplineName(2, $disciplineName)) as $r) {
-    $sheet = $excel->setActiveSheetIndexByName($r['name']);
-
-    foreach ($db->query(Query::agency()) as $r1) {
-        $sheet->setCellValueByColumnAndRow($r1['seq'], 1, $r1['id']);
-        $sheet->setCellValueByColumnAndRow($r1['seq'], 2, $r1['name']);
-        $sheet->setCellValueByColumnAndRow($r1['seq'], 61, $r1['id']);
-        $sheet->setCellValueByColumnAndRow($r1['seq'], 62, $r1['name']);
-    }
-
-    foreach ($db->query(Query::subDisciplineData($h, $disciplineName, $r['name'])) as $r1) {
-        $sheet->setCellValueByColumnAndRow($r1['col'], $r1['row'], $r1['percent']);
-    }
-
-    foreach ($db->query(Query::disciplineData2($h, $disciplineName)) as $r1) {
-        $sheet->setCellValueByColumnAndRow($r1['col'], $r1['row'], $r1['value']);
-    }
-}
-
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'); 
-header('Content-Disposition: attachment;filename="RAW '.$disciplineName.' '.$h2.'.xlsx"'); 
-header('Cache-Control: max-age=0'); 
-$writer = PHPExcel_IOFactory::createWriter($excel, 'Excel2007'); 
-$writer->save('php://output');
 ?>
 
 
